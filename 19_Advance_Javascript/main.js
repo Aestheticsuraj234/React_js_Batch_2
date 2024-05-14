cf121ac3c55f4e8247ddc821797383d82c986c5d
Aestheticsuraj234 <jhapriya458@gmail.com>
2024-05-14 20:09:08 +0530

    day-19

---
+++ 19_Advance_Javascript/main.js
@@ -0,0 +1,113 @@
+// // console.log('task-1')
+// // console.log('task-2')
+// // console.log('task-3')
+
+
+setTimeout(()=>{
+    console.log('task-1')
+} , 2000)
+
+// console.log('task-2')
+// console.log('task-3')
+
+
+
+// // fetch
+// // setTimeout
+// // Promise
+// // clearInterval
+// // setInterval
+// // XMLHttpRequest
+// // async/await
+// // axios
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// // function syncFunction() {
+// //     console.log("Task 1");
+// //     console.log("Task 2");
+// //     console.log("Task 3");
+// // }
+
+// // console.log("Before calling synchronous function");
+// // syncFunction();
+// // console.log("After calling synchronous function");
+
+
+// // setInterval
+// // setTimeout
+// // fetch
+// // axios
+// // Promise
+// // async / await 
+// // XMLHttpRequest
+
+
+// // setTimeout(()=> {
+// //     console.log("Task 1");
+// // }, 2000);
+
+// // setTimeout(()=>{
+// //     console.log("this is my task-2");
+// // },3000)
+
+
+// // setTimeout(()=>{
+// //     console.log("this is my task-3");
+// // },1000)
+
+
+// //  // // *sync and async?
+
+// // // // console.log("I am a sync code") 
+// // // // setTimeout(()=>{
+// // // //     console.log("I am a async code")
+// // // // } , 5000)
+// // // // c
+
+
+
+
+// // // // // *asynx js?
+// // // // // *js is not async by default
+
+
+
+// // // // Sync
+// // // console.log("Hello-world_1")
+// // // console.log("Hello-world_2")
+// // // console.log("Hello-world_3")
+// // // console.log("Hello-world_4")
+// // // console.log("Hello-world_5")
+
+// // // // async
+
+// // // console.log("task_1")
+// // setTimeout(()=>{
+// //     console.log("task_2") }, 2000)
+
+
+
+
+
+
