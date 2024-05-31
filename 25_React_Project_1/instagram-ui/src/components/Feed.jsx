09aeee424cf7993559288c39092fa5788528b3bd
Aestheticsuraj234 <jhapriya458@gmail.com>
2024-05-31 21:06:03 +0530

    useState

--- 25_React_Project_1/instagram-ui/src/components/Feed.jsx
+++ 25_React_Project_1/instagram-ui/src/components/Feed.jsx
@@ -3,12 +3,26 @@ import Stories from './Stories';
 import Posts from './Posts';
 
 const Feed = () => {
+
+// const [count , setCount] = useState(0)
+
+
+
+
+
   return (
     <section className='mx-4'>
       <main className="flex flex-col items-center gap-10 mx-4 my-2 overflow-x-scroll">
         <Stories />
       </main>
       <Posts />
+
+      {/* <div className="flex flex-row justify-center items-center gap-x-6 mt-5">
+        <button onClick={()=>setCount(count-1)}  className="px-4 py-4 bg-red-500 border-1 rounded-lg text-white">-</button>
+        <span className="text-3xl font-extrabold text-white">{count}</span>
+        <button onClick={()=>setCount(count+1)}  className='px-4 py-4 bg-green-500 border-1 rounded-lg text-white'>+</button>
+
+      </div> */}
 
     </section>
 
--- 25_React_Project_1/instagram-ui/src/components/Post.jsx
+++ 25_React_Project_1/instagram-ui/src/components/Post.jsx
@@ -1,11 +1,42 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { BiComment, BiHeart, BiShare } from 'react-icons/bi'
+import { FaHeart } from "react-icons/fa";
 import { BsThreeDots } from 'react-icons/bs'
 import { CiBookmark } from 'react-icons/ci'
-import { LuBookMarked } from 'react-icons/lu'
+import { IoBookmark } from "react-icons/io5";
+
 
 const Post = ({ userAvatar, username, updatedAt, location, PostUrl, likesNumber }) => {
+    const [isLiked, setIsLiked] = useState(false);
+    const [likeCount, setLikeCount] = useState(200);
+    const [isSaved, setIsSaved] = useState(false);
+
+
+
+    const onLikeToggle = () => {
+        // setIsLiked(!false); = true
+        // setIsLiked(!true); = false;
+        setIsLiked(!isLiked)
+
+        if (!isLiked) {
+            setLikeCount(likeCount + 1)
+        }
+        else {
+            setLikeCount(likeCount - 1)
+        }
+
+    }
+
+
+    const onSavedToggle = () => {
+        setIsSaved(!isSaved)
+    }
+
+
+
+
     return (
+
         <div className='max-w-[32rem] w-full text-white p-4 h-auto bg-transparent m-4 rounded-md flex flex-col'>
             {/* upper */}
             <div className="flex justify-between items-center">
@@ -34,17 +65,22 @@ const Post = ({ userAvatar, username, updatedAt, location, PostUrl, likesNumber 
                     src={PostUrl}
                     className='w-full h-auto object-cover aspect-video rounded-[24px]'
                     alt="post"
-                    
+
                 />
             </div>
             {/* lower */}
             <div className='flex flex-row justify-between items-center'>
-                <div className='flex flex-row justify-center items-center gap-4'>
-                    <BiHeart size={24} />
+                <div className='flex group   flex-row justify-center items-center gap-4'>
+
+                    <span onClick={onLikeToggle} className='flex group-hover:cursor-pointer  justify-center items-center gap-1 font-medium text-base'>
+                        {isLiked ? <FaHeart size={24} className="text-red-500" /> : <BiHeart size={24} />} {likeCount}k </span>
+
                     <BiComment size={24} />
                     <BiShare size={24} />
                 </div>
-                <CiBookmark size={24} />
+                <span onClick={onSavedToggle} className="hover:cursor-pointer">
+                    {isSaved ? <IoBookmark size={24} className='text-white' /> : <CiBookmark size={24} />}
+                </span>
             </div>
         </div>
     )
